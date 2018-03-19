ef07c008d3df6798e8f2d5732db0ea928fe8b110
Benson Tsai <ben7293@gmail.com>
2018-03-19 13:41:49 -0400

    Moved language detection to Google Translate API

--- translator_service/views.py
+++ translator_service/views.py
@@ -8,15 +8,14 @@ import json
 @csrf_exempt
 def translate(request):
     original_text = json.loads(request.body.decode('utf-8'))['Text']
-    lang = detect(original_text)
-    # fire an API request to translate on Google
+    # fire a API request to translate on Google
     translator = Translator()
-    translatedString = translator.translate(original_text, src=lang).text
+    translated = translator.translate(original_text)
     # insert into database
-    translation = Translations(original_text=original_text, original_lang=lang, translation=translatedString)
+    translation = Translations(original_text=original_text, original_lang=translated.src, translation=translated.text)
     translation.save()
     # return the result in JSON format
-    return JsonResponse({"original_text": original_text, "original_lang": lang, "translation": translatedString}, json_dumps_params={'ensure_ascii':False})
+    return JsonResponse({"original_text": original_text, "original_lang": translated.src, "translation": translated.text}, json_dumps_params={'ensure_ascii':False})
 
 def getTranslationRecords(request):
 	return JsonResponse(list(Translations.objects.values()), safe=False)    
